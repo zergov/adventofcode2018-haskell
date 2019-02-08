c9518d871229a555c34c6ca560f2cefe98396fe9
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-02-08 09:42:54 -0500

    day2

--- day2/day2.hs
+++ day2/day2.hs
@@ -6,16 +6,20 @@ countChar e = length . (filter (== e))
 countChars :: String -> [Int]
 countChars s = map (\ c -> countChar c s) alpha
 
-findPairs = length . (filter (==2)) . countChars
-findTrios = length . (filter (==3)) . countChars
+findKChars k = length . (filter (==k)) . countChars
 
 part1 :: [String] -> Int
 part1 ids = pairs * trios
   where
-    pairs = length $ filter (>0) $ (map findPairs ids)
-    trios = length $ filter (>0) $ (map findTrios ids)
+    pairs = length . (filter (>0)) $ map (findKChars 2) ids
+    trios = length . (filter (>0)) $ map (findKChars 3) ids
+
+differsBy1 :: String -> String -> Bool
+differsBy1 s1 s2 = ((length s1) - 1) == (length . filter comp $ zip s1 s2)
+  where
+    comp (a, b) = a == b
 
 main = do
-  input <- getContents
+  input <- readFile "input.txt"
   putStrLn "Part1: "
   print (part1 $ lines input)
