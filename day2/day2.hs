d3257e37ba7b9ec04d7dfb31c277548129539217
Jonathan Lalande <jonathan.lalande.1@ens.etsmtl.ca>
2019-03-11 17:20:41 -0400

    day2 ish

--- day2/day2.hs
+++ day2/day2.hs
@@ -14,10 +14,29 @@ part1 ids = pairs * trios
     pairs = length . (filter (>0)) $ map (findKChars 2) ids
     trios = length . (filter (>0)) $ map (findKChars 3) ids
 
-differsBy1 :: String -> String -> Bool
-differsBy1 s1 s2 = ((length s1) - 1) == (length . filter comp $ zip s1 s2)
+-- ("abcd", ["aecd", "abed", ...])
+-- ("aecd", ["abcd", "abed", ...])
+--
+--
+-- ("abcd", [])
+-- ("aecd", ["adcd"])
+--
+--
+-- "acd"
+
+type Prototype = String
+
+testinput :: [String]
+testinput = ["abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz"]
+
+fabricsMatchPairs :: [Prototype] -> [(Prototype, [Prototype])]
+fabricsMatchPairs fabrics = map (\fabric -> (fabric, filter (/= fabric) fabrics)) fabrics
+
+-- findCandidates :: Prototype -> [Prototype] -> [Prototype]
+-- findCandidates proto fabrics = filter (\(a, b) -> a /= b) $ map (\fabric -> zip proto fabric) fabrics
+findCandidates proto fabrics =  filter (\pairs -> length pairs == 1) $ map go $ map (\fabric -> zip proto fabric) fabrics
   where
-    comp (a, b) = a == b
+    go = filter (\(a, b) -> a /= b)
 
 main = do
   input <- readFile "input.txt"
